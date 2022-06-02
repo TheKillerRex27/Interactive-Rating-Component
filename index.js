083508c2e86188ae4397747dcaa8cc0ecbca5511
TheKillerRex27 <alexnwafor27@gmail.com>
2022-06-02 13:30:53 +0100

    Changed File Layout for Simplicity

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+let num;
+
 function onClick() {
   for (let i = 1; i < 6; i++) {
     document.getElementById("btn-" + i).addEventListener("click", (event) => {
-      localStorage.setItem("num", event.target.name);
+     num = event.target.name;
 
       for (let i = 1; i < 6; i++) {
         document.getElementById("btn-" + i).classList.remove("rating-button-clicked");
@@ -9,4 +11,10 @@ function onClick() {
       event.target.classList.add("rating-button-clicked");
     })
   }
+
+  document.getElementById("submit").addEventListener("click", event => {
+    document.getElementById("rating-state").style.display = "none";
+    document.getElementById("thank-you-state").classList.remove("rating-card-hidden");
+    document.getElementById("rating").innerHTML = num;
+  })
 }
